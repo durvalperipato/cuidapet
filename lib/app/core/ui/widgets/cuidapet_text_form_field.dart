b8dfc2d8a40a47b663e776a65c6abc4934e80d57
durvalperipato <durvalperipatoneto@gmail.com>
2022-07-27 19:55:53 -0300

    Add TextFormField Pattern

---
+++ lib/app/core/ui/widgets/cuidapet_text_form_field.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extension.dart';
+
+class CuidapetTextFormField extends StatelessWidget {
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  final String label;
+  final bool obscureText;
+  final ValueNotifier<bool> _obscureTextVN;
+
+  CuidapetTextFormField({
+    Key? key,
+    this.controller,
+    this.validator,
+    required this.label,
+    this.obscureText = false,
+  })  : _obscureTextVN = ValueNotifier<bool>(obscureText),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: _obscureTextVN,
+      builder: (context, obscureTextVNValue, __) => TextFormField(
+        controller: controller,
+        validator: validator,
+        obscureText: obscureTextVNValue,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            gapPadding: 0,
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          suffixIcon: obscureText
+              ? IconButton(
+                  onPressed: () {
+                    _obscureTextVN.value = !obscureTextVNValue;
+                  },
+                  color: context.primaryColor,
+                  icon: Icon(
+                    obscureTextVNValue ? Icons.lock : Icons.lock_open,
+                  ),
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
--- lib/modules/auth/login/login_page.dart
+++ lib/modules/auth/login/login_page.dart
@@ -1,7 +1,15 @@
+import 'package:cuidapet_mobile/app/core/ui/widgets/cuidapet_text_form_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +17,20 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login Page'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              CuidapetTextFormField(
+                label: 'Login',
+                obscureText: true,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
