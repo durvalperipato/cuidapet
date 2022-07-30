e36454f927452eabc52416963aaa60e04b21f964
durvalperipato <durvalperipatoneto@gmail.com>
2022-07-30 12:12:17 -0300

    Add Local Storage / Local Secure Storage

--- lib/modules/core/core_module.dart
+++ lib/modules/core/core_module.dart
@@ -1,3 +1,6 @@
+import 'package:cuidapet_mobile/app/core/local_storage/flutter_secure_storage/flutter_secure_storage_local_storage_impl.dart';
+import 'package:cuidapet_mobile/app/core/local_storage/local_storage.dart';
+import 'package:cuidapet_mobile/app/core/local_storage/shared_prefences/shared_preferences_local_storage_impl.dart';
 import 'package:cuidapet_mobile/app/core/logger/app_logger.dart';
 import 'package:cuidapet_mobile/app/core/logger/logger_app_logger_impl.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/dio/dio_rest_client.dart';
@@ -10,7 +13,9 @@ class CoreModule extends Module {
   List<Bind<Object>> get binds => [
         Bind.lazySingleton((i) => AuthStore(), export: true),
         Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true),
-        Bind.lazySingleton<AppLogger>((i) => LoggerAppLoggerImpl(),
-            export: true)
+        Bind.lazySingleton<AppLogger>((i) => LoggerAppLoggerImpl(), export: true),
+        Bind.lazySingleton<LocalStorage>((i) => SharedPreferencesLocalStorageImpl(), export: true),
+        Bind.lazySingleton<LocalSecureStorage>((i) => FlutterSecureStorageLocalStorageImpl(),
+            export: true),
       ];
 }
